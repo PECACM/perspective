8d99505b8d23a87e80fb5423647ca9e186ce2ee4
errpv78 <parikhgoyal13@gmail.com>
2022-02-19 12:01:43 +0530

    Perspective app created

---
+++ perspective/src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      Home
+      {/* Navbar (Logo, items, register/login) */}
+      {/* Picture carousel (Tagline, org by) */}
+      {/* Sponsered by */}
+      {/* About */}
+      {/* Conference committee */}
+      {/* Call for papers */}
+      {/* Important details */}
+      {/* Contact us (Footer) */}
+
+
+    </div>
+  )
+}
+
+export default Home
