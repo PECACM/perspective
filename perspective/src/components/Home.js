005a7d46ccbe4f0fad04e2807305eb394e236b15
Shayan <shayan.yaseen1@gmail.com>
2022-02-22 13:50:22 +0530

    theme

--- perspective/src/components/Home.js
+++ perspective/src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+//mui imports
+import { Typography } from "@mui/material";
+import { Box } from '@mui/system';
+import { Button } from '@mui/material';
+
 function Home() {
   return (
     <div>
-      Home
       {/* Navbar (Logo, items, register/login) */}
       {/* Picture carousel (Tagline, org by) */}
       {/* Sponsered by */}
@@ -12,10 +16,21 @@ function Home() {
       {/* Call for papers */}
       {/* Important details */}
       {/* Contact us (Footer) */}
-
-
+      <Box m={5}>
+        <Box mb={5}>
+          <Typography variant="h2">
+            Home Component
+          </Typography>
+        </Box>
+        <Button variant="contained" color="primary">
+          Primary Theme
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary Theme
+        </Button>
+      </Box>
     </div>
-  )
+  );
 }
 
 export default Home
--- perspective/src/index.js
+++ perspective/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from "./theme.js"
+ 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
