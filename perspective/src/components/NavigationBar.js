44a90a469d5d36d133f1233c34ecb5b709f20a34
Gaganpreet Khurana <54024846+GaganpreetKhurana@users.noreply.github.com>
2022-02-23 03:05:09 +0530

    Menu Working

--- perspective/src/components/NavigationBar.js
+++ perspective/src/components/NavigationBar.js
@@ -6,6 +6,11 @@ import Link from '@mui/material/Link';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
+
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
 import Fade from '@mui/material/Fade';
 import Menu from '@mui/material/Menu';
 import Logo from "../static/pec-logo.png";
@@ -15,12 +20,16 @@ const NavigationBar = () => {
 	const [anchorElAbout, setAnchorElAbout] = React.useState(null);
 	const [anchorElCommittee, setAnchorElCommittee] = React.useState(null);
 	const [anchorElDetails, setAnchorElDetails] = React.useState(null);
+	const [anchorMenu, setAnchorMenu] = React.useState('');
 	const openAbout = Boolean(anchorElAbout);
 	const openCommittee = Boolean(anchorElCommittee);
 	const openDetails = Boolean(anchorElDetails);
+	const openNav = Boolean(anchorElNav);
 	
 	
 	const handleOpenAbout = (event) => {
+		handleCloseDetails(event);
+		handleCloseCommittee(event);
 		setAnchorElAbout(event.currentTarget);
 	};
 	const handleCloseAbout = (event) => {
@@ -28,6 +37,8 @@ const NavigationBar = () => {
 	};
 	
 	const handleOpenCommittee = (event) => {
+		handleCloseAbout(event);
+		handleCloseDetails(event)
 		setAnchorElCommittee(event.currentTarget);
 	};
 	const handleCloseCommittee = (event) => {
@@ -35,10 +46,29 @@ const NavigationBar = () => {
 	};
 	
 	const handleOpenDetails = (event) => {
+		console.log(anchorMenu)
+		handleCloseCommittee(event);
+		handleCloseAbout(event);
 		setAnchorElDetails(event.currentTarget);
 	};
 	const handleCloseDetails = (event) => {
 		setAnchorElDetails(null);
+	};
+	
+	const handleOpenNav = (event) => {
+		setAnchorMenu('right')
+		handleCloseCommittee(event);
+		handleCloseAbout(event);
+		handleCloseDetails(event);
+		setAnchorElNav(event.currentTarget);
+	};
+	const handleCloseNav = (event) => {
+		setAnchorMenu('')
+		
+		handleCloseCommittee(event);
+		handleCloseAbout(event);
+		handleCloseDetails(event);
+		setAnchorElNav(null);
 	};
 	return (
 		<AppBar position="static">
@@ -69,7 +99,8 @@ const NavigationBar = () => {
 							TransitionComponent={Fade}
 						>
 							<MenuItem onClick={handleCloseAbout}><Link href="#" underline="hover">About Institute</Link></MenuItem>
-							<MenuItem onClick={handleCloseAbout}><Link href="#" underline="hover">About Conference</Link></MenuItem>
+							<MenuItem onClick={handleCloseAbout}><Link href="#" underline="hover">About
+								Conference</Link></MenuItem>
 						</Menu>
 						<Button
 							key="Commitee"
@@ -86,8 +117,10 @@ const NavigationBar = () => {
 							onClose={handleCloseCommittee}
 							TransitionComponent={Fade}
 						>
-							<MenuItem onClick={handleCloseCommittee}><Link href="#" underline="hover">Organizing Committee</Link></MenuItem>
-							<MenuItem onClick={handleCloseCommittee}><Link href="#" underline="hover">Advisory Board</Link></MenuItem>
+							<MenuItem onClick={handleCloseCommittee}><Link href="#" underline="hover">Organizing
+								Committee</Link></MenuItem>
+							<MenuItem onClick={handleCloseCommittee}><Link href="#" underline="hover">Advisory
+								Board</Link></MenuItem>
 						</Menu>
 						<Button
 							sx={{my: 2, color: 'white', display: 'block'}}
@@ -107,9 +140,13 @@ const NavigationBar = () => {
 							open={openDetails}
 							onClose={handleCloseDetails}
 							TransitionComponent={Fade}
+							anchorOrigin={{ horizontal: {anchorMenu}, vertical: 'bottom' }}
+						
 						>
-							<MenuItem onClick={handleCloseDetails}><Link href="#" underline="hover">Important Dates</Link></MenuItem>
-							<MenuItem onClick={handleCloseDetails}><Link href="#" underline="hover">Payment	Gateway</Link></MenuItem>
+							<MenuItem onClick={handleCloseDetails}><Link href="#" underline="hover">Important
+								Dates</Link></MenuItem>
+							<MenuItem onClick={handleCloseDetails}><Link href="#" underline="hover">Payment
+								Gateway</Link></MenuItem>
 						</Menu>
 						<Button
 							sx={{my: 2, color: 'white', display: 'block'}}
@@ -122,48 +159,60 @@ const NavigationBar = () => {
 					</Box>
 					
 					
-					{/*<Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>*/}
-					{/*	<IconButton*/}
-					{/*		size="large"*/}
-					{/*		aria-label="account of current user"*/}
-					{/*		aria-controls="menu-appbar"*/}
-					{/*		aria-haspopup="true"*/}
-					{/*		onClick={handleOpenMenu("Nav")}*/}
-					{/*		color="inherit"*/}
-					{/*	>*/}
-					{/*		<MenuIcon/>*/}
-					{/*	</IconButton>*/}
-					{/*	<Menu*/}
-					{/*		id="menu-appbar"*/}
-					{/*		anchorEl={anchorElNav}*/}
-					{/*		anchorOrigin={{*/}
-					{/*			vertical: 'bottom',*/}
-					{/*			horizontal: 'left',*/}
-					{/*		}}*/}
-					{/*		keepMounted*/}
-					{/*		transformOrigin={{*/}
-					{/*			vertical: 'top',*/}
-					{/*			horizontal: 'left',*/}
-					{/*		}}*/}
-					{/*		open={Boolean(anchorElNav)}*/}
-					{/*		onClose={handleCloseMenu("Nav")}*/}
-					{/*		sx={{*/}
-					{/*			display: {xs: 'block', md: 'none'},*/}
-					{/*		}}*/}
-					{/*	>*/}
-					{/*		{menu.map((item) => (*/}
-					{/*			<MenuItem key={item} onClick={handleCloseMenu("Nav")}>*/}
-					{/*				<Typography textAlign="center">{item}</Typography>*/}
-					{/*			</MenuItem>*/}
-					{/*		))}*/}
-					{/*	</Menu>*/}
-					{/*</Box>*/}
-					{/*<Box*/}
-					{/*	component="img"*/}
-					{/*	sx={{flexGrow: 0.01, display: {xs: 'flex', md: 'none'} }}*/}
-					{/*	alt="PEC Logo"*/}
-					{/*	src={Logo}*/}
-					{/*/>*/}
+					<Box
+						component="img"
+						sx={{flexGrow: 0.01, display: {xs: 'flex', md: 'none'}}}
+						alt="PEC Logo"
+						src={Logo}
+					/>
+					<Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+						<IconButton
+							size="large"
+							aria-label="account of current user"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleOpenNav}
+							color="inherit"
+						>
+							<MenuIcon/>
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: 'bottom',
+								horizontal: 'left',
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'left',
+							}}
+							open={openNav}
+							onClose={handleCloseNav}
+							sx={{
+								display: {xs: 'block', md: 'none'},
+							}}
+						>
+							<MenuItem key="Home" onClick={handleCloseNav}>
+								<Link href="#" underline="hover">Home</Link>
+							</MenuItem>
+							<MenuItem
+								key="Details"
+								onClick={handleOpenDetails}
+								sx={{my: 2, display: 'block'}}
+							>Important Details
+							</MenuItem>
+							<MenuItem key="About Us" onClick={handleCloseNav}>
+								<Link href="#"  underline="hover">Contact Us</Link>
+							</MenuItem>
+							<MenuItem key="Login" onClick={handleCloseNav}>
+								<Link href="#" underline="hover">Register / Login</Link>
+							</MenuItem>
+						
+						</Menu>
+					</Box>
+				
 				
 				</Toolbar>
 			</Container>
