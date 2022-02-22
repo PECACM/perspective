814cea182f431c9eb512f030c71fc33454ce3041
Gaganpreet Khurana <54024846+GaganpreetKhurana@users.noreply.github.com>
2022-02-22 16:22:55 +0530

    Basic Navbar Edited using MUI

--- perspective/src/components/NavigationBar.js
+++ perspective/src/components/NavigationBar.js
@@ -1,122 +1,111 @@
-import React, {Component} from "react";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
 
-import Navbar from "reactjs-navbar";
-import logo from "../static/pec-logo.png";
-import {TailSpin} from "react-loader-spinner";
+import Logo from "../static/pec-logo.png";
 
-import "reactjs-navbar/dist/index.css";
+const menu = ["Home", "About", "Conference Committee", "Call for Papers", "Important Details", "Contact Us", "Register/Login"]
+const about = ['About Institute', 'About Conference'];
+const committee = ['Organizing Committee', 'Advisory Board'];
+const details = ['Important Dates', 'Payment Gateway'];
 
-class NavigationBar extends Component{
+
+const NavigationBar = () => {
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
 	
-	render(){
-		return (
-			<Navbar
-				logo={logo}
-				loader={<TailSpin color="#00BFFF" height={80} width={80}/>}
-				menuItems={[
-					{
-						title: "Home",
-						isAuth: true,
-						onClick: () => {
-							// What you want to do...
-							alert("Its coffee time...");
-						},
-					},
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
+	
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+	
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
+	
+	return (
+		<AppBar position="static">
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<Box
+						component="img"
+						sx={{mr: 2, display: {xs: 'none', md: 'flex'}}}
+						alt="PEC Logo"
+						src={Logo}
+						noWrap
+					/>
 					
-					{
-						title: "About",
-						isAuth: true,
-						subItems: [
-							{
-								title: "About Institute",
-								isAuth: true,
-								onClick: () => {
-									// What you want to do...
-									alert("Its About Institue time...");
-								},
-							},
-							{
-								title: "About Conference",
-								isAuth: true,
-								onClick: () => {
-									// What you want to do...
-									alert("Its About Conference time...");
-								},
-							}
-						],
-					},
-					{
-						title: "Conference Commitee",
-						isAuth: true,
-						subItems: [
-							{
-								title: "Organizing Committee",
-								isAuth: true,
-								onClick: () => {
-									// What you want to do...
-									alert("Its Org Com time...");
-								},
-							},
-							{
-								title: "Advisory Board",
-								isAuth: true,
-								onClick: () => {
-									// What you want to do...
-									alert("Its Advisory Board time...");
-								},
-							}
-						],
-					},
-					{
-						title: "Call for Papers",
-						isAuth: true,
-						onClick: () => {
-							// What you want to do...
-							alert("Its Call for Papers time...");
-						},
-					},
-					{
-						title: "Important Details",
-						isAuth: true,
-						subItems: [
-							{
-								title: "Important Dates",
-								isAuth: true,
-								onClick: () => {
-									// What you want to do...
-									alert("I need another cup Dates of coffee...");
-								},
-							},
-							{
-								title: "Payment Gateway",
-								isAuth: true,
-								onClick: () => {
-									// What you want to do...
-									alert("I need another cup Payment of coffee...");
-								},
-							},
-						],
-					},
-					{
-						title: "Contact Us",
-						isAuth: true,
-						onClick: () => {
-							// What you want to do...
-							alert("I need another cup Contact US of coffee...");
-						},
-					},
-					{
-						title: "Register/Login",
-						isAuth: true,
-						onClick: () => {
-							// What you want to do...
-							alert("I need another cup Register/Login of coffee...");
-						},
-					},
-				]}
-			/>
-		);
-	}
-}
-
-export default NavigationBar
+					<Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+						<IconButton
+							size="large"
+							aria-label="account of current user"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleOpenNavMenu}
+							color="inherit"
+						>
+							<MenuIcon/>
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: 'bottom',
+								horizontal: 'left',
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'left',
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: {xs: 'block', md: 'none'},
+							}}
+						>
+							{menu.map((item) => (
+								<MenuItem key={item} onClick={handleCloseNavMenu}>
+									<Typography textAlign="center">{item}</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+					<Box
+						component="img"
+						sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}
+						alt="PEC Logo"
+						src={Logo}
+					/>
+					<Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+						{menu.map((items) => (
+							<Button
+								key={items}
+								onClick={handleCloseNavMenu}
+								sx={{my: 2, color: 'white', display: 'block'}}
+							>
+								{items}
+							</Button>
+						))}
+					</Box>
+				
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
+};
+export default NavigationBar;
